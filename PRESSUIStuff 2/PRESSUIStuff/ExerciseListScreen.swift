a7dafc911603663f9e206ae80ccc2744b8c8ab5f
kenzierichards <mrrichards@muhlenberg.edu>
2019-11-13 19:56:53 -0500

    fixed bugs!

---
+++ PRESSUIStuff 2/PRESSUIStuff/ExerciseListScreen.swift
@@ -0,0 +1,30 @@
+//
+//  ExerciseListScreen.swift
+//  PRESSUIStuff
+//
+//  Created by Ettinger003 iMac1 on 11/13/19.
+//  Copyright © 2019 Ettinger003 iMac2. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseListScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
